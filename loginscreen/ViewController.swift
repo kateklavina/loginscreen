d80cbc592f5880021719ad3f48f431b9614a2e6f
Kate Klavina <kate.klavina97@gmail.com>
2019-11-05 17:33:07 +0200

    Initial Commit

---
+++ loginscreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loginscreen
+//
+//  Created by Dace Laizane on 05/11/2019.
+//  Copyright © 2019 Kate Klavina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
